cb759abe55b97c01a8ef264f7757905eb366304f
Ronan Legin <ronan@narval3.narval.calcul.quebec>
2023-08-13 16:02:28 -0400

    Renamed rng key variable name.

--- src/slicgw/sampling/utils.py
+++ src/slicgw/sampling/utils.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 
 def get_sample_fn(step_fn, step_burn_fn, score_fn, mass_matrix=None):
 
-  def sample_fn(key, current_position, epsilon, n, burn_in = 0):
+  def sample_fn(rng, current_position, epsilon, n, burn_in = 0):
       
       current_score = score_fn(current_position)
       current_state = (current_position, current_score)
@@ -20,9 +20,9 @@ def get_sample_fn(step_fn, step_burn_fn, score_fn, mass_matrix=None):
       total_proposed = 0
       for step in (pbar := tqdm(range(n))):
           if step < burn_in:
-                current_state, _, key = step_burn_fn(key, current_state, epsilon, score_fn, mass_matrix)
+                current_state, _, rng = step_burn_fn(rng, current_state, epsilon, score_fn, mass_matrix)
           else:
-                current_state, accepted, key = step_fn(key, current_state, epsilon, score_fn, mass_matrix)
+                current_state, accepted, rng = step_fn(rng, current_state, epsilon, score_fn, mass_matrix)
                 accepted_count += jnp.sum(accepted)  # Count accepted samples
                 total_proposed += accepted.size  # Count total
                 pbar.set_description(f"Average acceptance = {accepted_count / total_proposed:.2f}")
